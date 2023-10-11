f30c6f7f82d9bd6fc343f3ba47d6281b328ff2df
caiomelotec <caio.melotec@icloud.com>
2023-10-11 17:57:47 +0200

    recipe state +  handleInputChanges + handleSubmit

--- client/src/pages/AddRecipe.jsx
+++ client/src/pages/AddRecipe.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const AddRecipe = () => {
   const createEmptyIngredient = () => ({
@@ -9,8 +11,13 @@ export const AddRecipe = () => {
   });
 
   const [inputList, setInputList] = useState([createEmptyIngredient()]);
+  const [recipe, setRecipe] = useState({ recipe_name: "" });
 
-  const handleInputChange = (e, index) => {
+  const handleInputChange = (e) => {
+    setRecipe((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleInputIngredientNameChange = (e, index) => {
     const newList = [...inputList];
     newList[index].ingredient_name = e.target.value;
     setInputList(newList);
@@ -35,12 +42,33 @@ export const AddRecipe = () => {
       setInputList(newList);
     }
   };
-  console.log(inputList);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const requestData = { recipe, inputList };
+      await axios.post("http://localhost:4000/addrecipe", requestData);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // console.log(inputList);
 
   return (
     <div className="forms-wrapper-container">
       <div className="form-container">
         <form>
+          <label>Rezeptname</label>
+          <input
+            type="text"
+            placeholder="Rezeptname"
+            name="recipe_name"
+            id="recipe_name"
+            onChange={handleInputChange}
+          />
           <div className="dynamic-inputstitle-div">
             <p className="menge-title">Menge</p>
             <p>Einheit</p>
@@ -63,6 +91,7 @@ export const AddRecipe = () => {
                 value={ingredient.unit}
                 className="unity-select"
               >
+                <option value="">keine Einheit</option>
                 <option value="Becher">Becher</option>
                 <option value="Beet/e">Beet/e</option>
                 <option value="Beutel">Beutel</option>
@@ -80,23 +109,33 @@ export const AddRecipe = () => {
                 <option value="Eimer">Eimer</option>
                 <option value="einige">einige</option>
                 <option value="einige Stiele">einige Stiele</option>
+                <option value="g">g</option>
               </select>
               <input
                 type="text"
                 value={ingredient.ingredient_name}
-                onChange={(e) => handleInputChange(e, index)}
+                onChange={(e) => handleInputIngredientNameChange(e, index)}
                 placeholder="Zutatenname"
                 name="ingredient_name"
                 id="ingredient_name"
               />
-              <button
-                className="remove-inputs-btn"
-                onClick={() => handleRemoveField(index)}
-              >
-                Löschen
-              </button>
+              {inputList.length > 1 && (
+                <button
+                  className="remove-inputs-btn"
+                  onClick={() => handleRemoveField(index)}
+                >
+                  Löschen
+                </button>
+              )}
             </div>
           ))}
+          <button
+            type="submit"
+            className="add-inputs-btn"
+            onClick={handleSubmit}
+          >
+            Rezept einreichen
+          </button>
         </form>
         <button className="add-inputs-btn" onClick={handleAddField}>
           Weitere Zutaten hinzufügen
