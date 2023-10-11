e1bb1f50252fdd005665e669e9f78a75dc01fbd3
caiomelotec <caio.melotec@icloud.com>
2023-10-12 01:24:13 +0200

    fetching ingredients and recipe data

--- client/src/pages/RecipeDetails.jsx
+++ client/src/pages/RecipeDetails.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const RecipeDetails = () => {
   const location = useLocation();
   const recipeId = location.pathname.split("/")[2];
 
-  // console.log(recipeId);
+  const [recipe, setRecipe] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    const fetchingRecipeData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:4000/recipes`);
+        setRecipe(response.data);
+      } catch (err) {
+        console.error(err, "Error fetching the recipe data");
+      }
+    };
+    fetchingRecipeData();
+  }, []);
+
+  useEffect(() => {
+    const fetchingIngredientsData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/recipes/${recipeId}/ingredients`
+        );
+        setIngredients(response.data);
+      } catch (err) {
+        console.error(err, "Error fetching the ingredients data");
+      }
+    };
+    fetchingIngredientsData();
+  }, []);
+
+  console.log(ingredients);
   return <div>RecipeDetails</div>;
 };
