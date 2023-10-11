a19b94f5a932a36dd873afd6d9f3b818289636c6
caiomelotec <caio.melotec@icloud.com>
2023-10-11 23:46:09 +0200

    fetching recipes data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.error(err, "Error fetching the recipes");
+      }
+    };
+    fetchRecipes();
+  }, []);
+  console.log(recipes);
+  return (
+    <div className="recipes-wrapper-container">
+      <div className="recipes-container">
+        {recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h1>{recipe.recipe_name}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
