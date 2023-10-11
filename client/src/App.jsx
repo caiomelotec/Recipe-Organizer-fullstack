86476692a880c596ec2524074afd35bc75a60c1c
caiomelotec <caio.melotec@icloud.com>
2023-10-11 17:54:44 +0200

    home router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { AddRecipe } from "./pages/AddRecipe.jsx";
+import { Home } from "./pages/Home.jsx";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} /> */}
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />  */}
         <Route path="/addrecipe" element={<AddRecipe />} />
       </Routes>
     </div>
