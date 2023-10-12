e7e22a74719ba6d637efd6eb867748936ee23882
caiomelotec <caio.melotec@icloud.com>
2023-10-12 02:03:41 +0200

    rendering ingredients and recipe data

--- client/src/pages/RecipeDetails.jsx
+++ client/src/pages/RecipeDetails.jsx
@@ -7,14 +7,14 @@ export const RecipeDetails = () => {
   const location = useLocation();
   const recipeId = location.pathname.split("/")[2];
 
-  const [recipe, setRecipe] = useState([]);
+  const [recipes, setRecipes] = useState([]);
   const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
     const fetchingRecipeData = async () => {
       try {
         const response = await axios.get(`http://localhost:4000/recipes`);
-        setRecipe(response.data);
+        setRecipes(response.data);
       } catch (err) {
         console.error(err, "Error fetching the recipe data");
       }
@@ -36,6 +36,33 @@ export const RecipeDetails = () => {
     fetchingIngredientsData();
   }, []);
 
+  const recipe = recipes.find((recipe) => recipe.recipe_id == recipeId); // single
+
+  if (!recipe) {
+    return <div>Loading...</div>; // render the loading component if project is not found
+  }
+  // console.log(recipe);
+
+  // console.log(recipeId);
   console.log(ingredients);
-  return <div>RecipeDetails</div>;
+  return (
+    <div>
+      <div>
+        <h1>{recipe.recipe_name}</h1>
+        <img src={recipe.imgUrl} alt="" />
+        {ingredients.map((ingredient) => (
+          <div
+            className="ingredients-div-container"
+            key={ingredient.ingredient_id}
+          >
+            <div className="ingredient-item-div">
+              <p>{ingredient.quantity}</p>
+              <p>{ingredient.unit}</p>
+              <p>{ingredient.ingredient_name}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
