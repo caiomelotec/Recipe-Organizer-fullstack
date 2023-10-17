97ff8cb49062bf76c8bfc29e07387cba7deff78c
caiomelotec <caio.melotec@icloud.com>
2023-10-17 22:59:41 +0200

    new Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import { AddRecipe } from "./pages/AddRecipe.jsx";
 import { Home } from "./pages/Home.jsx";
 import { RecipeDetails } from "./pages/RecipeDetails.jsx";
+import { Header } from "./componentes/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes/:id" element={<RecipeDetails />} />
