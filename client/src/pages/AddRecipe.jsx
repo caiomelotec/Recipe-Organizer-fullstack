ee69b47324a74563555fe800a6a36228e2418166
caiomelotec <caio.melotec@icloud.com>
2023-10-31 00:12:24 +0100

    separate

--- client/src/pages/AddRecipe.jsx
+++ client/src/pages/AddRecipe.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 import { DynamicInputs } from "../componentes/DynamicInputs";
 import { RecipeStepFormControll } from "../componentes/RecipeStepFormControll";
 import { AddRecipeFormErrors } from "../componentes/AddRecipeFormErrors";
+import { AddRecipeFormFirstSection } from "../componentes/AddRecipeFormFirstSection";
 
 export const AddRecipe = () => {
   const navigate = useNavigate();
@@ -95,6 +96,7 @@ export const AddRecipe = () => {
         {/* form */}
         <form>
           {formStep >= 0 && (
+            // first form step
             <section
               className="section-form-one"
               style={
@@ -103,46 +105,9 @@ export const AddRecipe = () => {
                   : { visibility: "hidden", position: "absolute" }
               }
             >
-              <div className="recipe-name">
-                <label>Rezeptname</label>
-                <input
-                  type="text"
-                  placeholder="Rezeptname"
-                  name="recipe_name"
-                  id="recipe_name"
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-              <div className="img-url">
-                <label>Image URL</label>
-                <input
-                  type="text"
-                  name="imgUrl"
-                  id="imgUrl"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="portion">
-                <label>Portionen</label>
-                <div className="portion-info-div">
-                  <span>Das Rezept ist ausgelegt für</span>
-                  <input
-                    min={0}
-                    type="number"
-                    name="portion"
-                    id="portion"
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <span>Personen / Portionen.</span>
-                </div>
-              </div>
-              <div className="dynamic-inputstitle-div">
-                <p className="menge-title">Menge</p>
-                <p className="einheit-title">Einheit</p>
-                <p>Zutatenname</p>
-              </div>
+              <AddRecipeFormFirstSection
+                handleInputChange={handleInputChange}
+              />
               <DynamicInputs
                 inputList={inputList}
                 setInputList={setInputList}
