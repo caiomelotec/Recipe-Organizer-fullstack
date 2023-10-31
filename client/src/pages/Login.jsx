62714dec7f4a9a862d8dc4ca01aff1fafea5327a
caiomelotec <caio.melotec@icloud.com>
2023-10-31 16:48:29 +0100

    login page + style

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import "../styles/Login.css";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { MdOutlineMailOutline } from "react-icons/md";
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login-wrapper">
+      <div className="login-container">
+        <form className="login-form">
+          <div className="filter-div"></div>
+          <h1 className="login-title">Koch</h1>
+          <h3 className="login-subtitle">Jetzt einloggen</h3>
+          <div className="email-div">
+            <MdOutlineMailOutline className="email-icon-login" />
+            <input type="email" placeholder="Email" name="email" id="email" />
+          </div>
+          <div className="password-div">
+            <RiLockPasswordLine className="password-icon-login" />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              id="password"
+            />
+          </div>
+          <button className="login-page-btn">Einloggen</button>
+          <span className="loginlink">Du hast noch kein Koch-Konto?</span>
+          <span className="login-link">Neues Konto erstellen</span>
+        </form>
+      </div>
+    </div>
+  );
 };
