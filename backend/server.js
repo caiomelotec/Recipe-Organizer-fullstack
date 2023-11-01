aed32949a0e5975ebf8c9c19d33fedcdcf7e18e5
caiomelotec <caio.melotec@icloud.com>
2023-11-01 20:16:55 +0100

    auth routes

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ const app = express();
 const port = 4000;
 require("dotenv").config();
 const recipesRouters = require("./routes/recipes");
+const authRouters = require("./routes/auth");
 
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(cors());
 
 // routes
 app.use(recipesRouters);
+app.use(authRouters);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
