71f5ab82227af6ab46489a9fdfeea47214174a7f
caiomelotec <caio.melotec@icloud.com>
2023-11-03 00:25:45 +0100

    fetchuserById + handle errros

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export const Profile = () => {
-  return <div>Profile</div>;
+  let { userId } = useParams();
+  const [err, setErr] = useState(null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUserById = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:4000/api/usersbyid/${userId}`,
+          { withCredentials: true }
+        );
+        setUser(res.data);
+      } catch (err) {
+        setErr(err.response.data);
+        console.log(err.response.data);
+      }
+    };
+    if (userId) {
+      fetchUserById();
+    }
+  }, [userId]);
+
+  // handle errors
+  if (!user && err) {
+    return <h1>{err}</h1>;
+  } else if (!user) {
+    return <h1>Loarding...</h1>;
+  }
+
+  const userdata = user ? user : null;
+
+  return (
+    <div>
+      <h1>{userdata.firstname}</h1>
+    </div>
+  );
 };
