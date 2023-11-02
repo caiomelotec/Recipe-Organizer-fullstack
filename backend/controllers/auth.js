df17fab59e96c7591f23f0dc5d5d22cd6cefe1aa
caiomelotec <caio.melotec@icloud.com>
2023-11-02 01:59:54 +0100

    login controller logic

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,9 +5,9 @@ var jwt = require("jsonwebtoken");
 exports.register = (req, res) => {
   const { firstname, lastname, email, password } = req.body;
   //CHECK EXISTING USER
-  const q = "SELECT * FROM users WHERE email = ?";
+  const checkQuery = "SELECT * FROM users WHERE email = ?";
 
-  db.query(q, [email], (err, data) => {
+  db.query(checkQuery, [email], (err, data) => {
     if (err) return res.status(500).json(err);
     if (data.length) return res.status(409).json("Email bereits registriert!");
 
@@ -15,13 +15,39 @@ exports.register = (req, res) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
 
-    const q =
+    const insertQuery =
       "INSERT INTO users(`firstname`, `lastname`,`email`,`password`) VALUES (?)";
     const values = [firstname, lastname, email, hash];
 
-    db.query(q, [values], (err, data) => {
+    db.query(insertQuery, [values], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json("Der Benutzer wurde erstellt.");
     });
+  });
+};
+
+exports.login = (req, res) => {
+  const { email, pass } = req.body;
+  const q = "SELECT * FROM users WHERE email = ?";
+
+  db.query(q, [email], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length > 0) return res.status(404).json("User not found");
+
+    // check password
+    const isPasswordCorrect = bcrypt.compareSync(pass, data[0].password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong Email or Password");
+
+    const token = jwt.sign({ id: data[0].id }, process.env.JWTKEY);
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
   });
 };
