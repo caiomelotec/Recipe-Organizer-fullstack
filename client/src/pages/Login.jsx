efd38e491ac0d2fe1f2317f7976f2c9bcacff14b
caiomelotec <caio.melotec@icloud.com>
2023-11-02 02:42:08 +0100

    post login request

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import { AuthForm } from "../componentes/AuthForm";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"; // Import Navigate if not already imported
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Login = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
   const [inputs, setInputs] = useState({
     email: "",
     password: "",
@@ -11,7 +17,31 @@ export const Login = () => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  console.log(inputs);
+  const notifyError = () =>
+    toast.error(error, {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:4000/api/login", inputs, {
+        withCredentials: true,
+      });
+      navigate("/"); // Corrected navigation
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data);
+      notifyError();
+    }
+  };
 
   return (
     <AuthForm
@@ -23,6 +53,7 @@ export const Login = () => {
       linkText="Du hast noch kein Koch-Konto?"
       linkTo="/register"
       handleInputChange={handleInputChange}
+      handleSubmit={handleSubmit}
     />
   );
 };
