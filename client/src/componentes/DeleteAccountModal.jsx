e1460f4fb259d2d5eb93ad6bbd1f44abb6abd2a5
caiomelotec <caio.melotec@icloud.com>
2023-11-04 00:55:40 +0100

    delete user by id delete request

--- client/src/componentes/DeleteAccountModal.jsx
+++ client/src/componentes/DeleteAccountModal.jsx
@@ -1,13 +1,33 @@
+import axios from "axios";
 import React from "react";
+import { useAuthStore } from "../store/authStore";
+import { useNavigate } from "react-router-dom";
 
 export const DeleteAccountModal = ({ deletemodal, setDeleteModal }) => {
+  const { logout } = useAuthStore((state) => state);
+  const navigate = useNavigate();
+
+  const deleteUserById = async () => {
+    try {
+      await axios.delete("http://localhost:4000/deleteuser", {
+        withCredentials: true,
+      });
+      await logout();
+      navigate("/");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div
       className="delete-modal-container"
       style={deletemodal ? { top: "40%" } : { top: "-20rem" }}
     >
-      <h3>Sind Sie sicher, dass Sie Ihr Konto löschen wollen?</h3>
-      <button className="ja-btn">Ja</button>
+      <h3>Sind Sie sicher, dass Sie Ihr Konto löschen wollen?😭</h3>
+      <button className="ja-btn" onClick={deleteUserById}>
+        Ja
+      </button>
       <button className="nein-btn" onClick={() => setDeleteModal(false)}>
         Nein
       </button>
