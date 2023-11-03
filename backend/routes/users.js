5e59016a5bb4ab85f0541b487ed0a78ef51d6469
caiomelotec <caio.melotec@icloud.com>
2023-11-03 02:54:33 +0100

    router uploaduserimg post/put

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users");
+const multer = require("multer");
 
 router.get(
   "/api/usersbyid/:userId",
   usersController.checkUserIdMiddleware,
   usersController.getUserById
 );
+// UPLOAD USER IMG
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+router.post(
+  "/uploaduserprofileimg",
+  upload.single("file"),
+  usersController.uploadUserImg
+);
+
+router.put("/updateuserprofileimg", usersController.upDateUserImg);
 
 module.exports = router;
