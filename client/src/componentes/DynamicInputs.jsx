493a5ebccd241101a6e107dd5325ce2081a1d276
caiomelotec <caio.melotec@icloud.com>
2023-11-07 20:49:51 +0100

    handle dynamic inputs change

--- client/src/componentes/DynamicInputs.jsx
+++ client/src/componentes/DynamicInputs.jsx
@@ -1,59 +1,47 @@
 import React from "react";
 import unitsArray from "../data";
 
-// Function to generate a random unique ID
 function generateUniqueId() {
-  return Math.random().toString(36).substr(2, 9);
+  return Math.random().toString(36);
 }
 
 export const DynamicInputs = ({ inputList, setInputList }) => {
-  const handleInputChangeQuantity = (e, index) => {
-    const newList = [...inputList];
-    newList[index].quantity = e.target.value;
-    setInputList(newList);
-  };
-
-  const handleInputChangeUnit = (e, index) => {
-    const newList = [...inputList];
-    newList[index].unit = e.target.value;
-    setInputList(newList);
-  };
-
-  const handleInputIngredientNameChange = (e, index) => {
-    const newList = [...inputList];
-    newList[index].ingredient_name = e.target.value;
-    setInputList(newList);
+  const handleDynamicInputsChange = (e, index) => {
+    let data = [...inputList];
+    data[index][e.target.name] = e.target.value;
+    setInputList(data);
   };
 
   const handleRemoveField = (index) => {
-    if (inputList.length > 1) {
-      const newList = [...inputList];
-      newList.splice(index, 1);
-      setInputList(newList);
-    }
+    let data = [...inputList];
+    data.splice(index, 1);
+    setInputList(data);
   };
 
   return (
     <>
       {inputList.map((ingredient, index) => (
-        <div key={generateUniqueId()} className="dynamic-inputs-div">
+        <div
+          key={ingredient.ingredient_id ? ingredient.ingredient_id : index}
+          className="dynamic-inputs-div"
+        >
           <input
             type="text"
             value={ingredient.quantity}
             name="quantity"
             id="quantity"
             placeholder="Menge"
-            onChange={(e) => handleInputChangeQuantity(e, index)}
+            onChange={(e) => handleDynamicInputsChange(e, index)}
           />
           <select
             name="unit"
             id="unit"
-            onChange={(e) => handleInputChangeUnit(e, index)}
+            onChange={(e) => handleDynamicInputsChange(e, index)}
             value={ingredient.unit}
             className="unity-select"
           >
-            {unitsArray.map((unit, index) => (
-              <option key={index} value={unit}>
+            {unitsArray.map((unit, i) => (
+              <option key={i} value={unit} name="units" id="units">
                 {unit}
               </option>
             ))}
@@ -61,7 +49,7 @@ export const DynamicInputs = ({ inputList, setInputList }) => {
           <input
             type="text"
             value={ingredient.ingredient_name}
-            onChange={(e) => handleInputIngredientNameChange(e, index)}
+            onChange={(e) => handleDynamicInputsChange(e, index)}
             placeholder="Zutatenname"
             name="ingredient_name"
             id="ingredient_name"
@@ -70,6 +58,7 @@ export const DynamicInputs = ({ inputList, setInputList }) => {
             <button
               className="remove-inputs-btn"
               onClick={() => handleRemoveField(index)}
+              type="button"
             >
               Löschen
             </button>
