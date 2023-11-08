a00071f684a54f16dbf4b986d21b9d8f461768cc
caiomelotec <caio.melotec@icloud.com>
2023-11-08 20:42:18 +0100

    fetchShoppingListByUserId + router

--- backend/controllers/shoppingList.js
+++ backend/controllers/shoppingList.js
@@ -30,3 +30,23 @@ exports.addShoppingLList = (req, res) => {
     );
   });
 };
+
+exports.fetchShoppingListByUserId = (req, res) => {
+  const token = req.cookies.token;
+  if (!token) return res.status(403).json("Not Authorized");
+
+  jwt.verify(token, process.env.JWTKEY, (err, userInfo) => {
+    if (err) return res.status(405).json("Token is not valid");
+
+    const getQuery = "SELECT * FROM shoppinglist WHERE sp_uid = ?";
+
+    db.query(getQuery, [userInfo.id], (err, data) => {
+      if (err)
+        return res.status(405).json({ err: "error getting shoppinglist data" });
+
+      // const deserializedData = JSON.parse(data)
+
+      return res.send(data).status(200);
+    });
+  });
+};
--- backend/routes/shoppingList.js
+++ backend/routes/shoppingList.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const shoppingListController = require("../controllers/shoppingList");
 
+router.get(
+  "/getshoppinglistinfo",
+  shoppingListController.fetchShoppingListByUserId
+);
+
 router.post("/shoppingList", shoppingListController.addShoppingLList);
 
 module.exports = router;
