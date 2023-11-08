b2231103025aefed41c2b25f533bc52788dc4dc0
caiomelotec <caio.melotec@icloud.com>
2023-11-08 19:27:06 +0100

    shoppinglist page

--- client/src/pages/ShoppingList.jsx
+++ client/src/pages/ShoppingList.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import { ShoppingListAccordion } from "../componentes/ShoppingListAccordion";
+import "../styles/ShoppingList.css";
 
 export const ShoppingList = () => {
-  return <div>ShoppingList</div>;
+  return (
+    <div className="shoppinglist-wrapper">
+      <div className="shoppinglist-container">
+        <h2>Meine Einkaufsliste: </h2>
+        <ShoppingListAccordion />
+        {/*  */}
+      </div>
+    </div>
+  );
 };
