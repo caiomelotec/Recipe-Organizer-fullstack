9039beeafd03aa7d9e8d9de70e6c193d738356e7
caiomelotec <caio.melotec@icloud.com>
2023-11-09 20:47:05 +0100

    fix footer

--- client/src/componentes/Footer.jsx
+++ client/src/componentes/Footer.jsx
@@ -5,8 +5,8 @@ import "../styles/Footer.css";
 export const Footer = () => {
   return (
     <footer>
-      <h4>Made with love, React, Node, Express and MySQL</h4>
-      <h4>Developed and designed by Caio Melo</h4>
+      <h4>Made with love, React, Node, Express and MySQL.</h4>
+      <h4>Developed and designed by Caio Melo.</h4>
       <div className="footer-div">
         <span>Find me on: </span>
         <a href="https://github.com/caiomelotec?tab=repositories">
