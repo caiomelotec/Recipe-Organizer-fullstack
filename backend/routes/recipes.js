da9ac5839162473bc68897da1267c544cb18c2a7
caiomelotec <caio.melotec@icloud.com>
2023-11-10 21:24:17 +0100

    uploadRecipeImg router

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const recipesController = require("../controllers/recipes");
 
+const multer = require("multer");
+
 router.get("/recipes", recipesController.getRecipes);
 
 router.get(
@@ -19,5 +21,23 @@ router.post("/addrecipe", recipesController.AddRecipe);
 router.delete("/recipes/:id", recipesController.deleteRecipeById);
 
 router.put("/recipes/:id", recipesController.editRecipe);
+
+// UPLOAD USER IMG
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+router.post(
+  "/uploadrecipeimg",
+  upload.single("file"),
+  recipesController.uploadRecipeImg
+);
 
 module.exports = router;
