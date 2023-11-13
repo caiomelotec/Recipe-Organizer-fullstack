e7314fd674971a70a13ec8fe001d020b171f1423
caiomelotec <caio.melotec@icloud.com>
2023-11-13 21:47:53 +0100

    db  config

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 // DB Connection
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: process.env.HOST,
+  user: process.env.USERNAME,
   password: process.env.DB_PASSWORD,
-  database: "recipes",
+  database: process.env.DATABASE,
 });
 
 module.exports = db;
