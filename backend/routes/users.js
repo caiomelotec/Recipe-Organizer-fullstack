ebccd865f8ed6e057ee081e0c98644d1ddf5b0ac
caiomelotec <caio.melotec@icloud.com>
2023-11-15 14:00:05 +0100

    clear multer config from router

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users");
-const multer = require("multer");
+const upload = require("../util/multer");
 
 router.get(
   "/api/usersbyid/:userId",
   usersController.checkUserIdMiddleware,
   usersController.getUserById
 );
-// UPLOAD USER IMG
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "../client/public/upload");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-const upload = multer({ storage });
 
 router.post(
   "/uploaduserprofileimg",
